596ca41db99f310a6602c43454d2198fb57bfcde
Kunal Patel <iamkunalpatel1@gmail.com>
2021-03-14 18:00:08 +0530

    Add support for connStr via an env variable.

--- startup/db.js
+++ startup/db.js
@@ -4,7 +4,7 @@ const config = require('config');
 
 module.exports = function(){
     const db = config.get('db');
-    mongoose.connect(db,{useNewUrlParser:true,useUnifiedTopology:true})
+    mongoose.connect(db)
         .then(() => winston.info(`Connected to ${db}...`));
 }   
    
